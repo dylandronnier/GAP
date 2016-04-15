8e9f18cc78d97926a45e62aaa5ce2eb51debf6c2
Dylan <dylan.dronnier@laposte.net>
2016-04-15 16:26:07 +0200

    indentation

--- fastmath.py
+++ fastmath.py
@@ -2,19 +2,18 @@ import numba
 
 @numba.jit(nopython=True)
 def euclidian_norm(v):
-        return inner_product(v, v) ** 0.5
+    return inner_product(v, v) ** 0.5
 
 @numba.jit(nopython=True)
 def norm3d(v):
-        assert len(v) == 3
-return euclidian_norm(v)
+    assert len(v) == 3
+    return euclidian_norm(v)
         
 @numba.jit(nopython=True)
 def inner_product(v1, v2):
-        n = len(v1)
-        assert n == len(v2)
-	
-        r = 0.0
-        for i in xrange(0, n):
+    n = len(v1)
+    assert n == len(v2)
+    r = 0.0
+    for i in xrange(0, n):
         r += v1[i] * v2[i]
-        return r
+    return r
